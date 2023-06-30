9fc882642e1f3c80c9e2fde37e98a3ff994ba889
Chad Fusco <fusco@case.edu>
2023-06-29 20:36:04 -0700

    initialized Lab2

---
+++ Lab2/lab2_chadfusco.asm
@@ -0,0 +1,11 @@
+# Write comments in your program that state your name, the programming logic, and any assembly language instruction line level details you feel you'd like to explain how you are using the assembly language instructions to perform the calculation of the sum.
+
+# NAME:
+# -------------------
+# Chad Fusco
+
+# TASK:
+# -------------------
+# Write a program that finds the sum of the following integer values:  2, 4, 9, 16, 25, 36, 49, 64, 81, 100
+# Each integer value should be stored with its own label.
+# Calculate the sum by accessing each of the labels, adding the integer value of each label, and storing the sum of the integers in the register $t0.
