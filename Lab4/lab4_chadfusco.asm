ac2b809b0b00698d17ae19ee184a81a16a72f6d0
Chad Fusco <fusco@case.edu>
2023-07-03 20:45:41 -0700

    initialized Lab 4

---
+++ Lab4/lab4_chadfusco.asm
@@ -0,0 +1,17 @@
+# NAME:
+# -------------------
+# Chad Fusco
+
+# TASK:
+# -------------------
+# Using this label:
+# a:  .word 5
+# Write a program which uses branching instructions to perform the following high level language if...else if... else conditional logic:
+
+# if the value for the label a is less than 0
+#       assign the value of -1 to the register $t0
+# else if the value for the label a is equal to 0
+#       assign the value of 0 to the register $t0
+# else
+#       assign the value of 1 to the register $t0
+
